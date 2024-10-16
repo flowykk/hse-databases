13352981a161ec9f667c0e01795e1a6232b02979
Danila Rakhmanov <71427624+flowykk@users.noreply.github.com>
2024-10-16 15:29:42 +0300

    Update script.sql

--- hw6/Задание1/script.sql
+++ hw6/Задание1/script.sql
@@ -1,1 +1,16 @@
+-- Создание таблицы Subjects
+CREATE TABLE [dbo].[Subjects] (
+    [s_id]    INT           NOT NULL,
+    [subjectname] NVARCHAR(50) NULL,
+    PRIMARY KEY CLUSTERED ([s_id] ASC)
+);
+
+-- Создание таблицы StudentSubject
+CREATE TABLE [dbo].[StudentSubject] (
+    [st_id] INT NOT NULL,
+    [s_id]  INT NOT NULL,
+    PRIMARY KEY (st_id, s_id),
+    FOREIGN KEY (st_id) REFERENCES [dbo].[students](st_id) ON DELETE CASCADE,
+    FOREIGN KEY (s_id)  REFERENCES [dbo].[Subjects](s_id) ON DELETE CASCADE
+);
 
