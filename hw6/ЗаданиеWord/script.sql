30a74303581c4abc58cabcc7d9d3232a6d9e5077
Danila Rakhmanov <71427624+flowykk@users.noreply.github.com>
2024-10-16 16:01:34 +0300

    Update script.sql

--- hw6/ЗаданиеWord/script.sql
+++ hw6/ЗаданиеWord/script.sql
@@ -22,3 +22,36 @@ CREATE TABLE Events (
     CONSTRAINT FK_Worker FOREIGN KEY (W_id) REFERENCES Workers(W_id) ON DELETE CASCADE,
     CONSTRAINT FK_Operation FOREIGN KEY (Op_id) REFERENCES Operations(Op_id) ON DELETE CASCADE
 );
+
+-- Вставка тестовых значений в таблицы
+INSERT INTO Workers (W_id, lastname, firstname) VALUES (1, 'Doe', 'John')
+INSERT INTO Workers (W_id, lastname, firstname) VALUES (2, 'Smith', 'Anna')
+
+INSERT INTO Operations (Op_id, opname, cost) VALUES (1, 'Welding', 200.00)
+INSERT INTO Operations (Op_id, opname, cost) VALUES (2, 'Painting', 150.00)
+
+INSERT INTO Events (Ev_id, W_id, Op_id, [Date], Number) VALUES (1, 1, 1, '2024-10-15', 100)
+
+-- Удаление значений
+DELETE FROM Workers WHERE W_id = 1
+DELETE FROM Operations WHERE Op_id = 2
+
+-- Добавление ограничений на таблицу Events
+ALTER TABLE Events
+ADD CONSTRAINT constraint_check CHECK (Number > 0 AND Number < 300)
+
+ALTER TABLE Events
+ADD CONSTRAINT constraint_def DEFAULT 20 FOR Number
+
+-- Проверка созданных ограничений
+INSERT INTO Events (Ev_id, W_id, Op_id, [Date], Number) VALUES (1, 1, 1, '2024-10-15', 400)
+INSERT INTO Events (Ev_id, W_id, Op_id, [Date]) VALUES (4, 1, 1, '2024-10-15')
+
+-- Добавление нового столбца в таблицу Workers
+ALTER TABLE Workers ADD middname VARCHAR(30)
+
+-- Добавление нового FK к таблице Events
+ALTER TABLE Events ADD CONSTRAINT FK_new FOREIGN KEY (W_id) REFERENCES Workers(W_id) ON UPDATE CASCADE
+
+-- Изменение типа значений столбца Number
+ALTER TABLE Events ALTER COLUMN Number BIGINT
